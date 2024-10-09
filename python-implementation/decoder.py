1ef88c02bae188b328baf72a2f0175c1b3fe9bdd
besteguney <63497613+besteguney@users.noreply.github.com>
2024-10-09 20:20:23 +0200

    Initial decoder version.

--- python-implementation/decoder.py
+++ python-implementation/decoder.py
@@ -1,8 +1,8 @@
 from struct import unpack
-
+from huffman_table import HuffmanTable
 
 marker_mapping = {
-    0xffd8: "Start of Image",
+    0xffd8: "Start Marker",
     0xffe0: "Application Default Header",
     0xffdb: "Quantization Table",
     0xffc0: "Start of Frame",
@@ -14,28 +14,92 @@ marker_mapping = {
 
 class JPEG:
     def __init__(self, image_file):
-        with open(image_file, 'rb') as f:
-            self.img_data = f.read()
+        f = open(image_file, "rb")
+        self.img_data = f.read()
+        self.data_chunks = self.extract_chunks()
+        for k,v in self.data_chunks.items():
+            print(k,v)
+            print() 
+    
+    def extract_chunks(self):
+        data_chunks = {}
+        data = self.img_data
+        i = 0
+        # start_ptr_header = i
+        while i < len(data):
+            marker, = unpack(">H", data[i:i+2])
+            if marker == 0xffd8:
+                data_chunks[marker_mapping[marker]] = marker
+            elif marker == 0xffe0:
+                start_ptr_header = i
+                start_marker = marker
+            elif marker == 0xffdb:
+                if marker_mapping[start_marker] not in data_chunks:
+                    data_chunks[marker_mapping[start_marker]] = data[start_ptr_header:i]
+                    start_ptr_header = i
+                    start_marker = marker
+                else:
+                    data_chunks["Quant_Table_1"] = data[start_ptr_header:i]
+                    start_ptr_header = i
+                    start_marker = marker
+            elif marker == 0xffc0:
+                data_chunks["Quant_Table_2"] = data[start_ptr_header:i]
+                start_ptr_header = i
+                start_marker = marker
+            elif marker == 0xffc4:
+                if "Start of Frame" not in data_chunks:
+                    data_chunks[marker_mapping[start_marker]] = data[start_ptr_header:i]
+                    start_marker = marker
+                    start_ptr_header = i
+                elif "Huffman_Table_1" not in data_chunks:
+                    data_chunks["Huffman_Table_1"] = data[start_ptr_header:i]
+                    start_marker = marker
+                    start_ptr_header = i
+                elif "Huffman_Table_2" not in data_chunks:
+                    data_chunks["Huffman_Table_2"] = data[start_ptr_header:i]
+                    start_marker = marker
+                    start_ptr_header = i
+                elif "Huffman_Table_3" not in data_chunks:
+                    data_chunks["Huffman_Table_3"] = data[start_ptr_header:i]
+                    start_marker = marker
+                    start_ptr_header = i
+            elif marker == 0xffda:
+                data_chunks["Huffman_Table_4"] = data[start_ptr_header:i]
+                start_ptr_header = i
+                start_marker = marker
+            elif marker == 0xffd9:
+                #TODO: pass the last part
+                break
+            i = i + 1
+        return data_chunks
     
     def decode(self):
-        data = self.img_data
-        while(True):
-            marker, = unpack(">H", data[0:2])
-            print(marker_mapping.get(marker))
-            if marker == 0xffd8:
-                data = data[2:]
-            elif marker == 0xffd9:
-                return
-            elif marker == 0xffda:
-                data = data[-2:]
-            else:
-                lenchunk, = unpack(">H", data[2:4])
-                data = data[2+lenchunk:]            
-            if len(data)==0:
-                break        
+        huffman_tables = []
+        for i in range(1,5):
+            huffman_tables.append(HuffmanTable(self.data_chunks[f'Huffman_Table_{i}']))
+        print(huffman_tables)            
+
+
+    # def decode(self):
+    #     data = self.img_data
+    #     while(True):
+    #         marker, = unpack(">H", data[0:2])
+    #         print(marker_mapping.get(marker))
+    #         if marker == 0xffd8:
+    #             data = data[2:]
+    #         elif marker == 0xffd9:
+    #             return
+    #         elif marker == 0xffda:
+    #             data = data[-2:]
+    #         else:
+    #             lenchunk, = unpack(">H", data[2:4])
+    #             data = data[2+lenchunk:]            
+    #         if len(data)==0:
+    #             break        
 
 if __name__ == "__main__":
     img = JPEG('profile.jpg')
+
     img.decode()    
 
 # OUTPUT:
---
+++ python-implementation/huffman_table.py
@@ -0,0 +1,87 @@
+from struct import unpack
+
+class Stream:
+    def __init__(self, data):
+        self.data= data
+        self.pos = 0
+
+    def GetBit(self):
+        b = self.data[self.pos >> 3]
+        s = 7-(self.pos & 0x7)
+        self.pos+=1
+        return (b >> s) & 1
+
+    def GetBitN(self, l):
+        val = 0
+        for i in range(l):
+            val = val*2 + self.GetBit()
+        return val
+
+class HuffmanTable:
+    def __init__(self, data):
+        self.root=[]
+        self.elements = []
+        self.header, self.lengths, self.elements = self.extract_table(data)
+        print("**********")
+        print(self.elements)
+        self.GetHuffmanBits(self.lengths, self.elements)
+        print(self.elements)
+        print("___________")
+        print(self.root)
+
+    def extract_table(self, data):
+        offset = 0
+        data = data[4:]
+        header, = unpack("B",data[offset:offset+1])
+        offset += 1
+
+        # Extract the 16 bytes containing length data
+        lengths = unpack("BBBBBBBBBBBBBBBB", data[offset:offset+16]) 
+        offset += 16
+
+        # Extract the elements after the initial 16 bytes
+        elements = []
+        for i in lengths:
+            elements += (unpack("B"*i, data[offset:offset+i]))
+            offset += i 
+
+        print("Header: ",header)
+        print("lengths: ", lengths)
+        print("Elements: ", len(elements))
+        data = data[offset:]
+        return header, lengths, elements
+    
+    def BitsFromLengths(self, root, element, pos):
+        if isinstance(root,list):
+            if pos==0:
+                if len(root)<2:
+                    root.append(element)
+                    return True                
+                return False
+            for i in [0,1]:
+                if len(root) == i:
+                    root.append([])
+                if self.BitsFromLengths(root[i], element, pos-1) == True:
+                    return True
+        return False
+    
+    def GetHuffmanBits(self,  lengths, elements):
+        ii = 0
+        for i in range(len(lengths)):
+            for j in range(lengths[i]):
+                self.BitsFromLengths(self.root, elements[ii], i)
+                ii+=1
+
+    def Find(self,st):
+        r = self.root
+        while isinstance(r, list):
+            r=r[st.GetBit()]
+        return  r 
+
+    def GetCode(self, st):
+        while(True):
+            res = self.Find(st)
+            if res == 0:
+                return 0
+            elif ( res != -1):
+                return res
