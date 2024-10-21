ba9bc94e7262f98487fb13f7d5dd3ac84854d0bb
carlosfdezserrano <fdezserranocarlos@gmail.com>
2024-10-21 22:48:32 +0200

    IDCT first cuda implementation

---
+++ src/IDCT.h
@@ -0,0 +1,35 @@
+#include <cuda_runtime.h>
+#include <iostream>
+
+class IDCT {
+    private:
+        int[64] base = {0};
+        int idct_precision;
+        int[][] zigzag = {
+            {0, 1, 5, 6, 14, 15, 27, 28},
+            {2, 4, 7, 13, 16, 26, 29, 42},
+            {3, 8, 12, 17, 25, 30, 41, 43},
+            {9, 11, 18, 24, 31, 40, 44, 53},
+            {10, 19, 23, 32, 39, 45, 52, 54},
+            {20, 22, 33, 38, 46, 51, 55, 60},
+            {21, 34, 37, 47, 50, 56, 59, 61},
+            {35, 36, 48, 49, 57, 58, 62, 63}
+        };
+        float* idct_table;
+
+        void initialize_idct_table();
+
+    public:
+        int* rearrange_using_zigzag();
+        void perform_IDCT();
+
+        IDCT(){ 
+            idct_precision = 8;
+            void initialize_idct_table();
+        }
+
+        ~IDCT(){
+            cudaFree(base);
+            cudaFree(zigzag);
+        }
+}
