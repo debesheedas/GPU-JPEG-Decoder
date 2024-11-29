6a64a9e0a5d3b5a014754f582d2248a2658d96cf
carlosfdezserrano <fdezserranocarlos@gmail.com>
2024-11-29 20:48:10 +0100

    created the new folder

---
+++ cuda-implementation-unified/src/huffmanTree.h
@@ -0,0 +1,51 @@
+#ifndef HUFFMAN_TREE_H
+#define HUFFMAN_TREE_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "../utils/stream.h"
+
+/*
+    Struct representing the nodes constructing the HuffmanTree.
+    They carry the values in unsigned integers.
+*/
+struct HuffmanTreeNode {
+    HuffmanTreeNode* left;
+    HuffmanTreeNode* right;
+    uint8_t val;
+    int length;
+    bool isLeaf;
+
+    HuffmanTreeNode(uint8_t val, int length, bool isLeaf) {
+        this->val = val;
+        this->length = length;
+        this->isLeaf = isLeaf;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+/*
+    HuffmanTree class for creating the codes from the JPEG huffman tables.
+*/
+class HuffmanTree {
+    std::vector<HuffmanTreeNode*> nodes;
+    std::vector<uint8_t> bytes;
+    HuffmanTreeNode* root;
+    std::unordered_map<uint8_t, std::string> codes;
+
+    void createNodes();
+    bool addToTree(HuffmanTreeNode* root, HuffmanTreeNode* node, int position);
+    void decodeTree(HuffmanTreeNode* node, std::string currentString);
+    void clearTree(HuffmanTreeNode* node);  // Helper to clean up the tree
+
+    public:
+        HuffmanTree(const std::vector<uint8_t>& bytes);
+        ~HuffmanTree();
+        std::unordered_map<uint8_t, std::string> getCodes();
+        uint8_t traverseTree(HuffmanTreeNode* cur, Stream* st);
+        uint8_t getCode(Stream* st);
+};
+
+#endif
---
+++ cuda-implementation-unified/src/parser.h
@@ -0,0 +1,72 @@
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <unordered_map>
+// #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <filesystem>
+
+#include "idct.h"
+#include "huffmanTree.h"
+#include <cuda_runtime.h>
+
+#ifdef __APPLE__
+    // #include <filesystem>
+    namespace fs = std::__fs::filesystem;
+#else
+    // #include <filesystem>
+    namespace fs = std::filesystem;
+#endif
+
+const std::vector<uint16_t> MARKERS = {0xffd8, 0xffe0, 0xffdb, 0xffc0, 0xffc4, 0xffda};
+
+/*
+    Class for accessing the image channels of an image.
+*/
+struct ImageChannels {
+    std::vector<std::vector<int>> channels;
+
+    ImageChannels(int size)
+        : channels(6, std::vector<int>(size)) {}  // 6 channels, each with `size` elements
+
+    // Accessor methods to get a reference to a specific channel
+    std::vector<int>& getY() { return channels[0]; }
+    std::vector<int>& getCr() { return channels[1]; }
+    std::vector<int>& getCb() { return channels[2]; }
+    std::vector<int>& getR() { return channels[3]; }
+    std::vector<int>& getG() { return channels[4]; }
+    std::vector<int>& getB() { return channels[5]; }
+};
+
+class JPEGParser {
+    private:
+        // Parts of the jpeg file.
+        std::string filename;
+        uint8_t* readBytes;
+        uint8_t* applicationHeader;
+
+        std::vector<uint8_t*> quantTables;
+
+        uint8_t startOfFrame;
+        std::unordered_map<int,std::vector<uint8_t>> huffmanTables;
+        std::unordered_map<int,HuffmanTree*> huffmanTrees;
+        std::vector<uint8_t> startOfScan;
+        std::vector<uint8_t> imageData;
+        ImageChannels* channels;
+
+        // Image features.
+        int height;
+        int width;
+
+        double* idctTable;
+        int* zigzag;
+
+        // Methods for extracting and building blocks.
+        void buildMCU(int* arr, Stream* imageStream, int hf, int quant, int& oldcoeff);
+    public:
+        JPEGParser(std::string& imagePath);
+        ~JPEGParser();
+        void extract();
+        void decode();
+        void write();
+};
