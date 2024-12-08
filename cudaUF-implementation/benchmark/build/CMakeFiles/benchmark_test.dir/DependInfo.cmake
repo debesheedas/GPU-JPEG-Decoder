d0c500866fb7658db841ad8d980889b19452b9d2
carlosfdezserrano <fdezserranocarlos@gmail.com>
2024-12-08 14:10:48 +0100

    fast idct to gpu.

---
+++ cudaUF-implementation/benchmark/build/CMakeFiles/benchmark_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/benchmark/benchmark.cu" "CMakeFiles/benchmark_test.dir/benchmark.cu.o" "gcc" "CMakeFiles/benchmark_test.dir/benchmark.cu.o.d"
+  "/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/src/parser.cu" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/src/parser.cu.o" "gcc" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/src/parser.cu.o.d"
+  "/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/utils/utils.cu" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/utils/utils.cu.o" "gcc" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/utils/utils.cu.o.d"
+  "/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/src/huffmanTree.cpp" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/src/huffmanTree.cpp.o" "gcc" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/src/huffmanTree.cpp.o.d"
+  "/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/utils/stream.cpp" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/utils/stream.cpp.o" "gcc" "CMakeFiles/benchmark_test.dir/home/dphpc2024_jpeg_1/GPU-JPEG-Decoder/cudaU-implementation/utils/stream.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
