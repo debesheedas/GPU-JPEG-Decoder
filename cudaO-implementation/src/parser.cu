f15311f945d257e30c117a4a8c2c804b75b560e1
carlosfdezserrano <fdezserranocarlos@gmail.com>
2024-12-09 17:45:31 +0100

    fast idct to cudaO

--- cudaO-implementation/src/parser.cu
+++ cudaO-implementation/src/parser.cu
@@ -2,6 +2,102 @@
 
 __constant__ int initialZigzag[64]; 
 
+__device__ int clip(int value) {
+    if (value < -256) return -256;
+    if (value > 255) return 255;
+    return value;
+}
+
+__device__ void idctRow(int* block) {
+    int x0, x1, x2, x3, x4, x5, x6, x7;
+
+    // Shortcut: if all AC terms are zero, directly scale the DC term
+    if (!((x1 = block[4]<<11) | (x2 = block[6]) | (x3 = block[2]) | (x4 = block[1]) | (x5 = block[7]) | (x6 = block[5]) | (x7 = block[3]))) {
+        block[0] = block[1] = block[2] = block[3] = block[4] = block[5] = block[6] = block[7] = block[0]<<3;
+        return;
+    }
+    // Scale the DC coefficient
+    x0 = (block[0]<<11) + 128;
+
+    int x8 = C7 * (x4 + x5);
+    x4 = x8 + (C1 - C7) * x4;
+    x5 = x8 - (C1 + C7) * x5;
+    x8 = C3 * (x6 + x7);
+    x6 = x8 - (C3 - C5) * x6;
+    x7 = x8 - (C3 + C5) * x7;
+
+    x8 = x0 + x1;
+    x0 -= x1;
+    x1 = C6 * (x3 + x2);
+    x2 = x1 - (C2 + C6) * x2;
+    x3 = x1 + (C2 - C6) * x3;
+    x1 = x4 + x6;
+    x4 -= x6;
+    x6 = x5 + x7;
+    x5 -= x7;
+
+    x7 = x8 + x3;
+    x8 -= x3;
+    x3 = x0 + x2;
+    x0 -= x2;
+    x2 = (181 * (x4 + x5) + 128) >> 8;
+    x4 = (181 * (x4 - x5) + 128) >> 8;
+
+    block[0] = (x7 + x1) >> 8;
+    block[1] = (x3 + x2) >> 8;
+    block[2] = (x0 + x4) >> 8;
+    block[3] = (x8 + x6) >> 8;
+    block[4] = (x8 - x6) >> 8;
+    block[5] = (x0 - x4) >> 8;
+    block[6] = (x3 - x2) >> 8;
+    block[7] = (x7 - x1) >> 8;
+}
+
+__device__ void idctCol(int* block) {
+    int x0, x1, x2, x3, x4, x5, x6, x7;
+
+    // Shortcut: if all AC terms are zero, directly scale the DC term
+    if (!((x1 = (block[8*4]<<8)) | (x2 = block[8*6]) | (x3 = block[8*2]) | (x4 = block[8*1]) | (x5 = block[8*7]) | (x6 = block[8*5]) | (x7 = block[8*3]))) {
+        block[8*0] = block[8*1] = block[8*2] = block[8*3] = block[8*4] = block[8*5] = block[8*6] = block[8*7] = clip((block[8*0]+32)>>6);
+        return;
+    }
+    // Scale the DC coefficient
+    x0 = (block[8*0]<<8) + 8192;
+
+    int x8 = C7 * (x4 + x5) + 4;
+    x4 = (x8 + (C1 - C7) * x4) >> 3;
+    x5 = (x8 - (C1 + C7) * x5) >> 3;
+    x8 = C3 * (x6 + x7) + 4;
+    x6 = (x8 - (C3 - C5) * x6) >> 3;
+    x7 = (x8 - (C3 + C5) * x7) >> 3;
+    
+    x8 = x0 + x1;
+    x0 -= x1;
+    x1 = C6 * (x3 + x2) + 4;
+    x2 = (x1 - (C2 + C6) * x2) >> 3;
+    x3 = (x1 + (C2 - C6) * x3) >> 3;
+    x1 = x4 + x6;
+    x4 -= x6;
+    x6 = x5 + x7;
+    x5 -= x7;
+
+    x7 = x8 + x3;
+    x8 -= x3;
+    x3 = x0 + x2;
+    x0 -= x2;
+    x2 = (181 * (x4 + x5) + 128) >> 8;
+    x4 = (181 * (x4 - x5) + 128) >> 8;
+
+    block[8 * 0] = clip((x7 + x1) >> 14);
+    block[8 * 1] = clip((x3 + x2) >> 14);
+    block[8 * 2] = clip((x0 + x4) >> 14);
+    block[8 * 3] = clip((x8 + x6) >> 14);
+    block[8 * 4] = clip((x8 - x6) >> 14);
+    block[8 * 5] = clip((x0 - x4) >> 14);
+    block[8 * 6] = clip((x3 - x2) >> 14);
+    block[8 * 7] = clip((x7 - x1) >> 14);
+}
+
 __global__ void initializeIDCTTableKernel(double *dIdctTable, int numThreads)
 {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
@@ -199,10 +295,11 @@ __device__ int match_huffman_code(uint8_t* stream, int bit_offset, uint16_t* huf
             if ((extracted_bits >> (16 - huff_bits[i]) & mask) == huff_codes[i]) {
                 code = i;
                 length = huff_bits[i];
-                return;
+                return i;
             }
         }
     }
+    return -1;
 }
 
 __device__ int buildMCU(int* outBuffer, uint8_t* imageData, int bitOffset, uint8_t* quant, 
@@ -366,21 +463,26 @@ __global__ void decodeKernel(uint8_t* imageData, int* arr_l, int* arr_r, int* ar
 
         __syncthreads();
 
-        int threadPos = pixelIndex % 64;
-        int threadRow = threadPos / 8;
-        int threadCol = threadPos % 8;
-        double localSum_l = 0.0;
-        double localSum_r = 0.0;
-        double localSum_y = 0.0;
+        if (pixelIndex % 8 == 0) {
+            idctRow(zigzag_l + (blockIndex % 4) * 64 + threadIndexInBlock);
+            idctRow(zigzag_r + (blockIndex % 4) * 64 + threadIndexInBlock);
+            idctRow(zigzag_y + (blockIndex % 4) * 64 + threadIndexInBlock);
+        }
 
-        performIDCT(zigzag_l, idctTable, threadCol, threadRow, localSum_l, threadId - (threadId % 64));
-        performIDCT(zigzag_r, idctTable, threadCol, threadRow, localSum_r, threadId - (threadId % 64));
-        performIDCT(zigzag_y, idctTable, threadCol, threadRow, localSum_y, threadId - (threadId % 64));
+        __syncthreads();
 
 
-        arr_l[pixelIndex] = static_cast<int>(std::floor(localSum_l / 4.0));
-        arr_y[pixelIndex] = static_cast<int>(std::floor(localSum_y / 4.0));
-        arr_r[pixelIndex] = static_cast<int>(std::floor(localSum_r / 4.0));
+        if (pixelIndex % 64 < 8) {
+            idctCol(zigzag_l + (blockIndex % 4) * 64 + threadIndexInBlock);
+            idctCol(zigzag_r + (blockIndex % 4) * 64 + threadIndexInBlock);
+            idctCol(zigzag_y + (blockIndex % 4) * 64 + threadIndexInBlock);
+            
+        }
+
+        __syncthreads();
+        arr_l[pixelIndex] = zigzag_l[(blockIndex % 4)*64 + threadIndexInBlock];
+        arr_r[pixelIndex] = zigzag_r[(blockIndex % 4)*64 + threadIndexInBlock];
+        arr_y[pixelIndex] = zigzag_y[(blockIndex % 4)*64 + threadIndexInBlock];
 
         pixelIndex += blockDim.x * gridDim.x;
     }
